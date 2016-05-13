44897a9de93255399bda59ac21edb5bc9ca1838b
AbbasHassan <abbashassan01@outlook.com>
2016-05-13 10:15:50 +0500

    added gridview

--- app/src/main/java/com/example/maryamch/github/MainActivity.java
+++ app/src/main/java/com/example/maryamch/github/MainActivity.java
@@ -5,11 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    List<Product> productList;
+    GridView gvProducts;
+    ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        gvProducts = (GridView) findViewById(R.id.gridview_products);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +37,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        productList = new ArrayList<>();
+        productList.add(new Product("khaadi", R.mipmap.ic_launcher));
+        productList.add(new Product("nishat linen", R.mipmap.ic_launcher));
+        productList.add(new Product("J.", R.mipmap.ic_launcher));
+        productList.add(new Product("Sana Safina's", R.mipmap.ic_launcher));
+        productList.add(new Product("Maria B.", R.mipmap.ic_launcher));
+        productList.add(new Product("Nicky and Nina", R.mipmap.ic_launcher));
+        productList.add(new Product("HSY", R.mipmap.ic_launcher));
+        productList.add(new Product("Vaneez Lawn", R.mipmap.ic_launcher));
+        productList.add(new Product("Almira", R.mipmap.ic_launcher));
+
+        productAdapter = new ProductAdapter(MainActivity.this, productList);
+        gvProducts.setAdapter(productAdapter);
     }
 
     @Override
@@ -50,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void funcion1(){
+    private void funcion1() {
 
     }
 }
